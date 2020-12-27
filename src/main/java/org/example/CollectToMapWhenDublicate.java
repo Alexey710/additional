ad75ff1fa4f5edd9e6b4ed3db16937f52b88f78c
Aleksey Krylov <elsite2015@yandex.ru>
2020-12-27 13:43:09 +0400

    2.4. Сборка элементов. Сборка в Map, когда есть дубликаты

---
+++ src/main/java/org/example/CollectToMapWhenDublicate.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectToMapWhenDublicate {
+    public static Map<Integer, Integer> collect(Stream<Integer> data) {
+        return data
+                .collect(Collectors.toMap(k -> k, b -> b * b, (previous, current) -> previous ));
+    }
+}
